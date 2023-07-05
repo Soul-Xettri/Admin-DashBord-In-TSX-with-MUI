4a6f450f836a4eaed7e5268665cf86fd4d08ae8e
Soul-Xettri <rakeshchhetri420@gmail.com>
2023-07-05 21:09:03 +0545

    react vite app and mui package init

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import Home from "./Pages/Home/Home"
+import {
+  Route,
+  BrowserRouter,
+  Routes,
+} from "react-router-dom"; 
+import Login from "./Pages/Login/Login";
+import List from "./Pages/List/List";
+import Single from "./Pages/Single/Single";
+import New from "./Pages/New/New";
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/">
+        <Route index element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="users">
+          <Route index element={<List/>}/>
+          <Route path=":userId" element={<Single/>}/>
+          <Route  path="new" element={<New/>}/>
+        </Route>
+        <Route path="products">
+          <Route index element={<List/>}/>
+          <Route path=":productId" element={<Single/>}/>
+          <Route  path="new" element={<New/>}/>
+        </Route>
+      </Route>
+    </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
