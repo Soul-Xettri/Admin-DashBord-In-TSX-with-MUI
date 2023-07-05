0467984e677734e68c040e0eb684e554b799ae99
Soul-Xettri <rakeshchhetri420@gmail.com>
2023-07-05 21:10:18 +0545

    Home 50%

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,26 @@
+import Featured from "../../Components/Chats/Featured/Featured"
+import Normal from "../../Components/Chats/Normal/Normal"
+import Navbar from "../../Components/Navbar/Navbar"
+import Sidebar from "../../Components/Sidebar/Sidebar"
+import Widget from "../../Components/Widget/Widget"
+import "./Home.scss"
+
+export default function Home() {
+  return (
+    <div className="home">
+        <Sidebar/>
+        <div className="homeContainer"><Navbar/>
+        <div className="widgets">
+          <Widget type="user"/>
+          <Widget type="order"/>
+          <Widget type="earning"/>
+          <Widget type="balance"/>
+        </div>
+        <div className="charts">
+          <Featured/>
+          <Normal/>
+        </div>
+        </div>
+    </div>
+  )
+}
