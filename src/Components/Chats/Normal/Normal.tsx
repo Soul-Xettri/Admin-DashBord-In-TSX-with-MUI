78db30941336cfe436bc4f5483fc448851a831d9
Soul-Xettri <rakeshchhetri420@gmail.com>
2023-07-05 21:09:36 +0545

    Normal Chart

---
+++ src/Components/Chats/Normal/Normal.tsx
@@ -0,0 +1,65 @@
+import "./Normal.scss";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  // YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  {name:"January", total:1200},
+  {name:"February", total:2100},
+  {name:"March", total:800},
+  {name:"April", total:1600},
+  {name:"May", total:900},
+  {name:"June", total:1700},
+];
+
+export default function Normal() {
+  return (
+    <div className="normal">
+      <div className="title">Last 6 Months (Revenue)</div>
+      {" "}
+      <ResponsiveContainer width="100%" aspect={2/1}>
+        <AreaChart
+          width={730}
+          height={250}
+          data={data}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+        >
+          <defs>
+            <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+            {/* <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+            </linearGradient> */}
+          </defs>
+          <XAxis dataKey="name" stroke="gray" />
+          {/* <YAxis /> */}
+          <CartesianGrid strokeDasharray="3 3" className="chartGrid"/>
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="total"
+            stroke="#8884d8"
+            fillOpacity={1}
+            fill="url(#total)"
+          />
+          {/* <Area
+            type="monotone"
+            dataKey="pv"
+            stroke="#82ca9d"
+            fillOpacity={1}
+            fill="url(#colorPv)"
+          /> */}
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
