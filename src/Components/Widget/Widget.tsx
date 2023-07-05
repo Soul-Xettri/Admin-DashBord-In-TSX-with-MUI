b72f622114f0fe7ab9ea646c85dd654e57067a22
Soul-Xettri <rakeshchhetri420@gmail.com>
2023-07-05 21:10:00 +0545

    Widgets

---
+++ src/Components/Widget/Widget.tsx
@@ -0,0 +1,107 @@
+import "./Widget.scss";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
+
+interface WidgetData {
+  title: string;
+  isMoney: boolean;
+  link: string;
+  icon: JSX.Element;
+}
+
+interface WidgetProps {
+  type: string;
+}
+
+export default function Widget({ type }: WidgetProps) {
+  let data: WidgetData | undefined;
+
+  // temp data
+  const amount = 100;
+  const diff = 20;
+
+  switch (type) {
+    case "user":
+      data = {
+        title: "USERS",
+        isMoney: false,
+        link: "See all users",
+        icon: (
+          <PersonOutlineIcon
+            className="icon"
+            style={{ color: "crimson", backgroundColor: "rgba(255,0,0,0.2)" }}
+          />
+        ),
+      };
+      break;
+    case "order":
+      data = {
+        title: "ORDERS",
+        isMoney: false,
+        link: "View all orders",
+        icon: (
+          <ShoppingCartOutlinedIcon
+            className="icon"
+            style={{ color: "goldenrod", backgroundColor: "rgba(218,165,32,0.2)" }}
+          />
+        ),
+      };
+      break;
+    case "earning":
+      data = {
+        title: "EARNINGS",
+        isMoney: true,
+        link: "View net earnings",
+        icon: (
+          <MonetizationOnOutlinedIcon
+            className="icon"
+            style={{ color: "green", backgroundColor: "rgba(0,128,0,0.2)" }}
+          />
+        ),
+      };
+      break;
+    case "balance":
+      data = {
+        title: "BALANCE",
+        isMoney: true,
+        link: "See details",
+        icon: (
+          <AccountBalanceWalletOutlinedIcon
+            className="icon"
+            style={{ color: "purple", backgroundColor: "rgba(128,0,128,0.2)" }}
+          />
+        ),
+      };
+      break;
+    default:
+      break;
+  }
+
+  if (!data) {
+    // Handle the case when `data` is undefined (no matching type)
+    return null;
+  }
+
+  return (
+    <div className="widget">
+      <div className="left">
+        <span className="title">{data.title}</span>
+        <span className="counter">
+          {data.isMoney && "$"}
+          {amount}
+        </span>
+        <span className="link">{data.link}</span>
+      </div>
+      <div className="right">
+        <div className="percentage positive">
+          <KeyboardArrowUpIcon />
+          {diff} %
+        </div>
+        {data.icon}
+      </div>
+    </div>
+  );
+}
